74920b381ee7e7f0db25fa38bc4d97a0ad6a8aa4
Khushi Desai <65886886+khushidesai1@users.noreply.github.com>
2024-01-08 12:45:37 -0800

    Update renderer.py with the coloring code

--- nerf/renderer.py
+++ nerf/renderer.py
@@ -374,17 +374,17 @@ class NeRFRenderer(nn.Module):
                 target_locations[5] = torch.tensor([0.13780347, 0.0118743, 0.41263683]).to(xyzs.get_device())
                 car_locations[5] = torch.tensor([0.12936965, 0.01920666, 0.40456722]).to(xyzs.get_device()) 
 
-                # target_location = target_locations[3]
-                # distances = torch.norm(xyzs - target_location, dim=-1)
-                # close_to_target = distances < tolerance
+                target_location = target_locations[0]
+                distances = torch.norm(xyzs - target_location, dim=-1)
+                close_to_target = distances < tolerance
 
-                # rgbs[close_to_target] = color1
+                rgbs[close_to_target] = color1
 
-                # target_location = car_locations[3]
-                # distances = torch.norm(xyzs - target_location, dim=-1)
-                # close_to_target = distances < tolerance
+                target_location = car_locations[0]
+                distances = torch.norm(xyzs - target_location, dim=-1)
+                close_to_target = distances < tolerance
 
-                # rgbs[close_to_target] = color2
+                rgbs[close_to_target] = color2
 
                 if target_positions is not None:
                     target_location = target_locations[target_positions]
